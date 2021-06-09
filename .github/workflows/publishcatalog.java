58304713b52bf1047e48b07626b57e429310ad8d
George Gastaldi <gegastaldi@gmail.com>
2021-06-09 12:45:22 -0300

    Update https.protocols

--- .github/workflows/publishcatalog.java
+++ .github/workflows/publishcatalog.java
@@ -2,7 +2,7 @@
 //DEPS info.picocli:picocli:4.6.1
 //DEPS io.quarkus:quarkus-devtools-registry-client:2.0.0.CR3
 //DEPS org.eclipse.jgit:org.eclipse.jgit:5.11.0.202103091610-r
-//JAVA_OPTIONS "-Djava.util.logging.SimpleFormatter.format=%1$s [%4$s] %5$s%6$s%n" -Dhttps.protocols=TLSv1.1,TLSv1.2
+//JAVA_OPTIONS "-Djava.util.logging.SimpleFormatter.format=%1$s [%4$s] %5$s%6$s%n" -Dhttps.protocols=TLSv1,TLSv1.1,TLSv1.2
 //JAVA 11
 
 import java.io.IOException;

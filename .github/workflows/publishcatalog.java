5f7b2da1ac5e9b9c391fe47fdb670e07aa3033d1
George Gastaldi <gegastaldi@gmail.com>
2021-07-01 11:42:05 -0300

    Update to Quarkus 2.0.0.Final

--- .github/workflows/publishcatalog.java
+++ .github/workflows/publishcatalog.java
@@ -1,6 +1,6 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
 //DEPS info.picocli:picocli:4.6.1
-//DEPS io.quarkus:quarkus-devtools-registry-client:2.0.0.CR3
+//DEPS io.quarkus:quarkus-devtools-registry-client:2.0.0.Final
 //DEPS org.eclipse.jgit:org.eclipse.jgit:5.11.0.202103091610-r
 //JAVA_OPTIONS "-Djava.util.logging.SimpleFormatter.format=%1$s [%4$s] %5$s%6$s%n"
 //JAVA 11
